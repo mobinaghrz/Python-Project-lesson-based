811b0fa1dfc0120dc3b867c8771956e19001d41f
mobina <68993058+mobinaghrz@users.noreply.github.com>
2025-11-16 12:43:16 -0500

    Advance (#5)
    * Create IntoNumPyArray.py
    
    * Update IntoNumPyArray.py

--- NumPy/IntoNumPyArray.py
+++ NumPy/IntoNumPyArray.py
@@ -33,3 +33,32 @@ arr = np.array((1, 3, 2))
 print(arr)
 
 print("-----------------------------------------")
+
+# Accessing the array Index
+# In a numpy array, indexing or accessing the array index can be done in multiple ways.
+# To print a range of an array, slicing is done.
+# Slicing of an array is defining a range in a new array which is used to print a range of elements from the original array.
+# Since, sliced array holds a range of elements of the original array, 
+# modifying content with the help of sliced array modifies the original array content.
+
+print("Accessing the array Index\n")
+
+import numpy as np
+ 
+arr = np.array([[-1, 2, 0, 4],
+                [4, -0.5, 6, 0],
+                [2.6, 0, 7, 8],
+                [3, -7, 4, 2.0]])
+
+# Printing a range of Array
+# with the use of slicing method
+arr2 = arr[:2, ::2]
+print ("first 2 rows and alternate columns(0 and 2):\n", arr2)
+ 
+# Printing elements at
+# specific Indices
+arr3 = arr[[1, 1, 0, 3], 
+                [3, 2, 1, 0]]
+print ("\nElements at indices (1, 3), "
+    "(1, 2), (0, 1), (3, 0):\n", arr3)
+print("-----------------------------------------")
